81e847dfa3a7628bd7898a57642d5e63f2f9d022
ashhadmatlub <ashhadmatlub@gmail.com>
2026-05-02 20:58:44 +0500

    Under Process

--- week9 & 10/graph_list/graph_list.cpp
+++ week9 & 10/graph_list/graph_list.cpp
@@ -41,10 +41,20 @@ void addVertex(Vertex *&curr, int data)
 void display()
 {
     Vertex *curr;
+    // Edge *edgeCurr;
     curr = Graph;
     while (curr != NULL)
     {
-        cout << curr->data << "\n";
+        cout << curr->data;
+        if (curr->edgeList != NULL)
+        {
+            while (curr->edgeList != NULL)
+            {
+                cout << " => " << curr->edgeList->v->data;
+                curr->edgeList = curr->edgeList->next;
+            }
+        }
+        cout << "\n";
         curr = curr->next;
     }
     return;
@@ -77,7 +87,7 @@ void removeVertex(int data)
             prev->next = cur->next;
             cur = cur->next;
             delete (temp);
-            cout << data << "Deleted Successfully \n";
+            cout << data << " Deleted Successfully \n";
             return;
         }
         cur = cur->next;
@@ -86,8 +96,51 @@ void removeVertex(int data)
     cout << "Vertex doesn't exists\n";
 }
 
-void addEdge()
+void addEdge(int v1, int v2)
 {
+    Vertex *curr = Graph; // pointer
+    Vertex *ver1 = NULL;  // to hold the address
+    Vertex *ver2 = NULL;
+    while (curr != NULL)
+    {
+        if (curr->data == v1)
+        {
+            ver1 = curr;
+        }
+        if (curr->data == v2)
+        {
+            ver2 = curr;
+        }
+        curr = curr->next;
+    }
+    if (ver1 == NULL)
+    {
+        cout << "Vertex" << v1 << "doesn't exists, can't draw edge";
+        return;
+    }
+    if (ver2 == NULL)
+    {
+        cout << "Vertex" << v2 << "doesn't exists can't draw edge";
+        return;
+    }
+
+    Edge *temp = new Edge;
+    temp->v = ver2;
+    temp->next = NULL;
+
+    if (ver1->edgeList == NULL)
+    {
+        ver1->edgeList = temp;
+        return;
+    }
+    Edge *edgeCurr = ver1->edgeList;
+    while (edgeCurr != NULL)
+    {
+        edgeCurr = edgeCurr->next;
+    }
+    edgeCurr = temp;
+
+    cout << "Edge Drawn Successfully \n";
 }
 int main()
 {
@@ -119,7 +172,12 @@ int main()
         }
         else if (choice == 3)
         {
-            // Add Edge
+            int v1, v2;
+            cout << "Enter Vertex 1 : ";
+            cin >> v1;
+            cout << "Enter Vertex 2 : ";
+            cin >> v2;
+            addEdge(v1, v2);
         }
         else if (choice == 4)
         {
